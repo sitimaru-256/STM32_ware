02f0b38f18f80989b5a91e8acfb8510060eefe75
Sitimaru_256 <verderayo0703@outlook.jp>
2026-09-01 21:34:57 +0900

    Add files via upload

---
+++ PP-PWM_test/Core/Inc/lut.h
@@ -0,0 +1,28 @@
+/*
+ * lut.h
+ *
+ *  Created on: Aug 31, 2026
+ *      Author: sitimaru_256
+ */
+
+#ifndef INC_LUT_H_
+#define INC_LUT_H_
+
+extern const int _7alpha[509][7];
+extern const int _6alpha[509][6];
+extern const int _5alpha[509][5];
+extern const int _4alpha[509][4];
+extern const int _3alpha[509][3];
+extern const int _2alpha[509][2];
+extern const int _1alpha[509][1];
+extern const int _7alpha_pole[509];
+extern const int _6alpha_pole[509];
+extern const int _5alpha_pole[509];
+extern const int _4alpha_pole[509];
+extern const int _3alpha_pole[509];
+extern const int _2alpha_pole[509];
+extern const int _1alpha_pole[509];
+
+extern const int SYNC[5][66];
+
+#endif /* INC_LUT_H_ */
